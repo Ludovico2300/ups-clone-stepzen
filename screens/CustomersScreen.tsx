4928fc3271b7a92bf9022a69b7f7a6c3caf21b68
Ludovico Colucci <ludovicocolucci@gmail.com>
2023-10-06 09:09:12 +0200

    [END SESSION]

--- screens/CustomersScreen.tsx
+++ screens/CustomersScreen.tsx
@@ -1,13 +1,27 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import React from "react";
 import { useTailwind } from "tailwind-rn";
+import {
+  CompositeNavigationProp,
+  useNavigation,
+} from "@react-navigation/native";
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import { TabStackParamList } from "../navigatore/TabNavigator";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../navigatore/RootNavigator";
+
+export type CustomersScreenNvigationProp = CompositeNavigationProp<
+  BottomTabNavigationProp<TabStackParamList, "Customers">,
+  NativeStackNavigationProp<RootStackParamList>
+>;
 
 export default function CustomersScreen() {
   const tw = useTailwind();
+  const navigation = useNavigation();
 
   return (
-    <SafeAreaView>
+    <ScrollView>
       <Text style={tw("text-red-500")}>CustomersScreen</Text>
-    </SafeAreaView>
+    </ScrollView>
   );
 }
