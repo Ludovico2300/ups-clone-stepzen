0509dbc15e6cb15bfac1d6ec60ceb762a9d1b49f
Ludovico Colucci <ludovicocolucci@gmail.com>
2023-10-06 16:19:12 +0200

    [ADD] Image and Input in CustomersScreen

--- screens/CustomersScreen.tsx
+++ screens/CustomersScreen.tsx
@@ -1,5 +1,11 @@
-import { View, Text, SafeAreaView, ScrollView } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
+import React, { useLayoutEffect, useState } from "react";
 import { useTailwind } from "tailwind-rn";
 import {
   CompositeNavigationProp,
@@ -9,19 +15,39 @@ import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
 import { TabStackParamList } from "../navigatore/TabNavigator";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../navigatore/RootNavigator";
+import { Image } from "@rneui/themed";
+import { Input } from "@rneui/base";
 
-export type CustomersScreenNvigationProp = CompositeNavigationProp<
+export type CustomersScreenNavigationProp = CompositeNavigationProp<
   BottomTabNavigationProp<TabStackParamList, "Customers">,
   NativeStackNavigationProp<RootStackParamList>
 >;
 
 export default function CustomersScreen() {
   const tw = useTailwind();
-  const navigation = useNavigation();
+  const navigation = useNavigation<CustomersScreenNavigationProp>();
+  const [input, setInput] = useState<string>("");
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
 
   return (
-    <ScrollView>
-      <Text style={tw("text-red-500")}>CustomersScreen</Text>
+    <ScrollView style={{ backgroundColor: "#59C1CC" }}>
+      <Image
+        source={{ uri: "https://links.papareact.com/3jc" }}
+        containerStyle={tw("w-full h-64")}
+        PlaceholderContent={<ActivityIndicator />}
+      />
+
+      <Input
+        placeholder="Search by Customer"
+        value={input}
+        onChangeText={setInput}
+        containerStyle={tw("bg-white pt-5 pb-0 px-10")}
+      />
     </ScrollView>
   );
 }
